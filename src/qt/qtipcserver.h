77391a76af0df001872d99fab8942b27564d51a1
Tillkoeln <Till_Noske@msn.com>
2018-10-24 15:39:02 +0200

    Hello World

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define Jumbos Coin-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "Jumbos CoinURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
